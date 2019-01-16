c71b05ca1f447aae81939a520f902697dd99aa4a
djpark0402 <djpark0402@raonsecure.com>
2019-01-16 11:32:53 +0900

    함수타입 셈플 추가

---
+++ Swift/Menu/Closure/MyClass.swift
@@ -0,0 +1,13 @@
+//
+//  MyClass.swift
+//  SwiftTest
+//
+//  Created by dong jun park on 16/01/2019.
+//  Copyright © 2019 djpark. All rights reserved.
+//
+
+import UIKit
+
+class MyClass: NSObject {
+
+}
---
+++ Swift/Menu/FunctionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FunctionViewController.swift
+//  SwiftTest
+//
+//  Created by dong jun park on 16/01/2019.
+//  Copyright © 2019 djpark. All rights reserved.
+//
+
+import UIKit
+
+class FunctionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
