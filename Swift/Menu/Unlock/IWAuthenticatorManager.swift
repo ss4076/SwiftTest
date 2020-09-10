cc094a5c3a3173d6d60ab1dbc76cd07e7fc06598
ss4076 <ss4076@gmail.com>
2020-09-10 16:51:01 +0900

    IWAuthenticatiorManager 추가

---
+++ Swift/Menu/Unlock/IWAuthenticatorManager.swift
@@ -0,0 +1,24 @@
+//
+//  IWAuthenticatorManager.swift
+//  SwiftTest
+//
+//  Created by raonsecure on 2020/09/10.
+//  Copyright © 2020 djpark. All rights reserved.
+//
+
+import UIKit
+
+class IWAuthenticatorManager: NSObject {
+
+    func registration() -> Void {
+        
+    }
+    
+    func authentication() -> Void {
+        
+    }
+    
+    func deregistration() -> Void {
+        
+    }
+}
