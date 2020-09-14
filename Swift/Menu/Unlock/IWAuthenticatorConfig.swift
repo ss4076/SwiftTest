89dcefaf7a85bb956728810c66c15389feb7f8cc
ss4076 <ss4076@gmail.com>
2020-09-14 14:40:46 +0900

    manager reg, auth, dereg 개발 config setAuthMessage

---
+++ Swift/Menu/Unlock/IWAuthenticatorConfig.swift
@@ -0,0 +1,18 @@
+//
+//  IWAuthenticatorConfig.swift
+//  SwiftTest
+//
+//  Created by raonsecure on 2020/09/14.
+//  Copyright © 2020 djpark. All rights reserved.
+//
+
+import UIKit
+
+class IWAuthenticatorConfig: NSObject {
+
+    static func setAuthMessage() -> NSString {
+        
+        return ""
+    }
+    
+}
---
+++ Swift/Menu/Unlock/IWAuthenticatorManager.swift
@@ -0,0 +1,23 @@
+//
+//  IWAuthenticatorManager.swift
+//  SwiftTest
+//
+//  Created by raonsecure on 2020/09/14.
+//  Copyright © 2020 djpark. All rights reserved.
+//
+
+import UIKit
+
+class IWAuthenticatorManager: NSObject {
+
+    func reg() -> Void {
+        
+    }
+    func auth() -> Void {
+        
+    }
+    func dereg() -> Void {
+        
+    }
+    
+}
