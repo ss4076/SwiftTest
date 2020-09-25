d70c46fbdb0a60e369e3ce62b9020087d97d7fe5
ss4076 <ss4076@gmail.com>
2020-09-25 16:09:17 +0900

    ims-1 끝(1주일짜리)

--- Swift/AppDelegate.swift
+++ Swift/AppDelegate.swift
@@ -50,7 +50,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        // ims-1 (1주일 정도)
+        // ims-1 (1주일 정도) 끝
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
