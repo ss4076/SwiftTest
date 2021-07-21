6a7287da382e99f3c7ab7a63362d47d57b665b7a
djpark0402 <djpark0402@raoncorp.com>
2021-07-21 15:13:44 +0900

    세미나 시작

--- Swift/Menu/Unlock/IWAuthenticatorManager.swift
+++ Swift/Menu/Unlock/IWAuthenticatorManager.swift
@@ -9,32 +9,16 @@
 import UIKit
 
 class IWAuthenticatorManager: NSObject {
-
+    // 등록
     func reg() -> Void {
-        
-        // 코드를 구현하는 중...
-        // 코드1 구현중...
-        // reg 침범
-        // qa - reg 수정
-    }
-    func auth() -> Void {
-        // auth 코드1 추가
-        // qa auth 수정
-        // qa auth 수정2
-    }
-    func dereg() -> Void {
-        // 끝
-    }
-    func bugfix() ->Void {
-        // 수정함
-        // dereg 코드1 추가
-        // 예솔 코드 추가
 
     }
-    func one() -> Void {
+    // 인증
+    func auth() -> Void {
         
     }
-    func two() -> Void {
+    // 해지
+    func dereg() -> Void {
         
     }
 }
