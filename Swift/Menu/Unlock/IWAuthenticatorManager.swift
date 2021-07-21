1f53744e9851b1970cfc4ce2957d0dde9addcfb1
djpark0402 <djpark0402@raoncorp.com>
2021-07-21 15:16:45 +0900

    업무 분장

--- Swift/Menu/Unlock/IWAuthenticatorManager.swift
+++ Swift/Menu/Unlock/IWAuthenticatorManager.swift
@@ -11,13 +11,13 @@ import UIKit
 class IWAuthenticatorManager: NSObject {
     // 등록
     func reg() -> Void {
-
+        // 기능1 개발
     }
-    // 인증
+    // 인증 - 지원
     func auth() -> Void {
         
     }
-    // 해지
+    // 해지 - 진호
     func dereg() -> Void {
         
     }
